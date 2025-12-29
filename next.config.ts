c22f4480f0485aff4808329ff193c9080f8c3877
monitus2022 <monitus202201@proton.me>
2025-12-29 21:29:38 +0800

    redeploy to reflect secret update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true,
-  basePath: '/music',
+  trailingSlash: true
 };
 
 export default nextConfig;
